87e3b266e4452ab010326c2a318db146f5182d71
ppaulis <ppaulis@gmail.com>
2019-06-18 13:40:38 +0200

    TMS-152 working on creator in SPL

---
+++ Constants/Spl.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Cinexpert\Constants;
+
+class Spl
+{
+    const SPL_CREATOR_SPL_BUILDER         = 'CXP-eTMS-SPLBuilder';
+    const SPL_CREATOR_DYNAMIC_SPL_BUILDER = 'CXP-eTMS-DynamicSPLBuilder';
+}
