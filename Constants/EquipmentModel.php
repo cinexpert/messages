411344b8a82e6d4840f3302e008d1affc81d1fea
Pascal Paulis <ppaulis@gmail.com>
2020-01-14 15:15:46 +0100

    NTE-731 added missing property for equipment models

---
+++ Constants/EquipmentModel.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Cinexpert\Constants;
+
+class EquipmentModel
+{
+    const TYPE_PLAYER    = 'type_player';
+    const TYPE_LMS       = 'type_lms';
+    const TYPE_EQUIPMENT = 'type_equipment';
+}
