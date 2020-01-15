18a4d78548f9a5b8fc67b2400e3e3287c90042c6
Pascal Paulis <ppaulis@gmail.com>
2020-01-15 14:12:50 +0100

    NTE-731 working on equipment models

--- Constants/EquipmentModel.php
+++ Constants/EquipmentModel.php
@@ -7,4 +7,11 @@ class EquipmentModel
     const TYPE_PLAYER    = 'player';
     const TYPE_LMS       = 'lms';
     const TYPE_EQUIPMENT = 'equipment';
+
+    const EQUIPMENT_MODEL_TYPES =
+        [
+            self::TYPE_EQUIPMENT,
+            self::TYPE_PLAYER,
+            self::TYPE_LMS,
+        ];
 }
